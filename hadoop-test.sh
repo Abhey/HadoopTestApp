ae18d986c233886aa59aeda27e733c4169e2cdc8
Abhey Rana <abheyranacool123.ar@gmail.com>
2019-04-27 17:54:06 +0530

    Fixed directory navigation error

--- hadoop-test.sh
+++ hadoop-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Create input directory on HDFS
 hadoop fs -mkdir -p input
 
 # Put input files to HDFS
-hdfs dfs -put ./input/* input
+hdfs dfs -put $DIR'/data.txt' input
 
 # Run wordcount
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-2.7.2-sources.jar org.apache.hadoop.examples.WordCount input output
